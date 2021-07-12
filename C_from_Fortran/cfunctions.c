8b5794faf9cc598b27bd2d5558f13b0271250408
vijay gopal chilkuri <vijay.gopal.c@gmail.com>
2021-07-12 19:25:09 +0530

    Working on Fortran from C.

--- C_from_Fortran/cfunctions.c
+++ C_from_Fortran/cfunctions.c
@@ -2,31 +2,12 @@
 
 double sum4d_all(double *vec2d, int64_t rows,int64_t cols){
     double result=0.0;
-    for(int i = 0; i < rows; i++)
-        for(int j = 0; j < cols; j++)
+    for(int j = 0; j < cols; ++j)
+        for(int i = 0; i < rows; ++i)
             printf("---\t %14.5f\n",vec2d[j*rows + i]);
 //          printf("---\t %d\n",vec2d[i + j*rows]);
-    for(int i = 0; i < rows; i++)
-        for(int j = 0; j < cols; j++)
-            result += vec2d[j*rows + i];
-
-    return(result);
-}
-
-void mapsum4d(double *vec2d, int64_t rows, int64_t cols, double *res){
-    *res = sum4d_all(vec2d, rows, cols);
-}
-
-#include <stdint.h>
-
-double sum4d_all(double *vec2d, int64_t rows,int64_t cols){
-    double result=0.0;
-    for(int i = 0; i < rows; i++)
-        for(int j = 0; j < cols; j++)
-            printf("---\t %14.5f\n",vec2d[j*rows + i]);
-//          printf("---\t %d\n",vec2d[i + j*rows]);
-    for(int i = 0; i < rows; i++)
-        for(int j = 0; j < cols; j++)
+    for(int j = 0; j < cols; ++j)
+        for(int i = 0; i < rows; ++i)
             result += vec2d[j*rows + i];
 
     return(result);
--- C_from_Fortran/fmain.f90
+++ C_from_Fortran/fmain.f90
@@ -16,84 +16,34 @@
       use cfunctions_intvec4d
       implicit none
       integer*8 :: rows = 10
+      integer*8 :: colsall = 4
       integer*8 :: cols = 2
-      real*8, dimension(2,2,10, 2) :: vec2d
+      real*8, dimension(10,4,2,2) :: vec2d
       real*8 :: res = 0
       integer :: i
       integer :: j,k,l
       integer :: count=0
-      do k=1,2
-         do l=1,2
+      do k=1, 2
+         do l=1, 2
             count = 0
-            do i = 1,rows
-               do j = 1, cols
-                  vec2d(k,l,i,j) = count*1.0
+            do j = 1, colsall
+               do i = 1, rows
+                  vec2d(i,j,l,k) = count*1.0d0
+                  !print *,vec2d(k,l,i,j)
                   count = count + 1
-                  print *,vec2d(k,l,i,j)
                end do
             end do
          end do
       end do
       count = 0
-      do i = 1,rows
-         do j = 1, cols
-            count = count + vec2d(1,1,i,j)
+      do j = 1, colsall
+         do i = 1, rows
+            print *,vec2d(i,j,1,1)
+            res = res + vec2d(i,j,1,1)
          end do
       end do
       print *,rows,cols,res, count
-      call cmapsum4d(vec2d(1,1,:,:), rows, cols, res)
-      call cmapsum4d(vec2d(1,2,:,:), rows, cols, res)
-      call cmapsum4d(vec2d(2,1,:,:), rows, cols, res)
-      call cmapsum4d(vec2d(2,2,:,:), rows, cols, res)
-      print *,rows, cols, res
-      end program main
-
-      module cfunctions_intvec4d
-      use, intrinsic :: ISO_C_BINDING
-      interface
-         subroutine cmapsum4d(vec2d, rows, cols, res) bind(C, name='mapsum4d')
-         import C_INT64_T, C_DOUBLE
-         integer(kind=C_INT64_T), value :: rows
-         integer(kind=C_INT64_T), value :: cols
-         real(kind=C_DOUBLE)        :: vec2d(rows,cols)
-         real(kind=C_DOUBLE)        :: res
-         end subroutine cmapsum4d
-      end interface
-      end module cfunctions_intvec4d
-
-      program main
-      use, intrinsic :: ISO_C_BINDING
-      use cfunctions_intvec4d
-      implicit none
-      integer*8 :: rows = 10
-      integer*8 :: cols = 2
-      real*8, dimension(2,2,10, 2) :: vec2d
-      real*8 :: res = 0
-      integer :: i
-      integer :: j,k,l
-      integer :: count=0
-      do k=1,2
-         do l=1,2
-            count = 0
-            do i = 1,rows
-               do j = 1, cols
-                  vec2d(k,l,i,j) = count*1.0
-                  count = count + 1
-                  print *,vec2d(k,l,i,j)
-               end do
-            end do
-         end do
-      end do
-      count = 0
-      do i = 1,rows
-         do j = 1, cols
-            count = count + vec2d(1,1,i,j)
-         end do
-      end do
-      print *,rows,cols,res, count
-      call cmapsum4d(vec2d(1,1,:,:), rows, cols, res)
-      call cmapsum4d(vec2d(1,2,:,:), rows, cols, res)
-      call cmapsum4d(vec2d(2,1,:,:), rows, cols, res)
-      call cmapsum4d(vec2d(2,2,:,:), rows, cols, res)
+      res = 0.0d0
+      call cmapsum4d(vec2d(:,:,1,1), rows, colsall, res)
       print *,rows, cols, res
       end program main
